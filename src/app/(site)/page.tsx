3fea19902d63713435d4373e882b26891709ff2e
Jason Donmoyer <jasondonmoyer@Mac.lan>
2026-03-06 04:43:56 -0500

    Added services section

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,24 +1,48 @@
 import { createServerSupabaseClient } from '@/lib/supabase-server.js'
 import Hero from '@/components/site/Hero'
+import Services from '@/components/site/Services'
 
 export default async function HomePage() {
     const supabase = await createServerSupabaseClient()
 
-    const { data, error} = await supabase
-        .from('content_blocks')
-        .select('*')
-        .eq('id', 'hero')
+    // const { data, error} = await supabase
+    //     .from('content_blocks')
+    //     .select('*')
+    //     .eq('id', 'hero')
     
-        if (error) {
-            console.error("There was an error fetchikng data:", error)
-            return (
-                <p>There was an error loading this page.</p>
-            )
-        }
+    //     if (error) {
+    //         console.error("There was an error fetching data:", error)
+    //         return (
+    //             <p>There was an error loading this page.</p>
+    //         )
+    //     }
+
+    const [
+        { data: heroData, error: heroError },
+        { data: servicesData, error: servicesError },
+        { data: testimonialsData, error: testimonialsError },
+        { data: pricingData, error: pricingError },
+        { data: aboutData, error: aboutError },
+    ] = await Promise.all([
+        supabase.from('content_blocks').select('*').eq('id', 'hero'),
+        supabase.from('services').select('*').order('order_index'),
+        supabase.from('testimonials').select('*').order('order_index'),
+        supabase.from('pricing_cards').select('*').order('order_index'),
+        supabase.from('content_blocks').select('*').eq('id', 'about'),
+    ])
+    if (heroError) return <p>Failed to load page.</p>
+    if (servicesError) return <p>Failed to load page.</p>
+    if (testimonialsError) return <p>Failed to load page.</p>
+    if (pricingError) return <p>Failed to load page.</p>
+    if (aboutError) return <p>Failed to load page.</p>
 
     return (
         <div>
-            <Hero heroData={data[0]}></Hero>
+            <Hero heroData={heroData[0]}></Hero>
+            <Services servicesData={servicesData}></Services>
+            {/* <Testimonials testimonialData={testimonialData}></Testimonials> */}
+            {/* <Pricing pricingData={pricingData}></Pricing> */}
+            {/* <About aboutData={aboutData[0]}></About> */}
         </div>
     )
 }
---
+++ src/components/site/Services.jsx
@@ -0,0 +1,39 @@
+import * as Icons from 'lucide-react'
+
+export default function Services({ servicesData }) {
+    
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-8 py-8 bg-brand-silver">
+        {servicesData.map(service => {
+            const Icon = Icons[service.icon]
+            return (
+                <div 
+                    key={service.id}
+                    className="text-center flex flex-col px-4 py-6 text-white bg-brand-blue mx-8 my-6 rounded-2xl shadow"   
+                >
+                    <h1 className="text-3xl font-bold mb-1.5">{service.name}</h1>
+                    <h2 className="mb-3">{service.description}</h2>
+                    <div className="flex items-center justify-center">
+                        {service.is_emergency
+                        ? <p className="text-white bg-brand-orange font-bold rounded p-5">Emergency Service</p>
+                        : <Icon size={30}></Icon>
+                        }  
+                    </div>
+                </div>
+                )
+            }
+        )}
+        </div>
+    )
+
+
+    // return (
+    //     <div
+    //         {servicesData.map(service => {
+    //             <h1>{service.name}</h1>
+    //         })}
+    //     >
+
+    //     </div>
+    // )
+}
