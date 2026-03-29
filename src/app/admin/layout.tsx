73cce74f757b4df74b61ac1d09980473c1be2e02
Jason Donmoyer <jasondonmoyer@Jasons-MacBook-Air.local>
2026-03-29 12:31:37 -0400

    Added SEO to layout pages and imported favicon and OG images

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,4 +1,12 @@
+import type { Metadata } from "next";
 import AdminNav from '@/components/admin/AdminNav'
+
+export const metadata: Metadata = {
+    robots: {
+        index: false,
+        follow: false
+    }
+}
 
 export default function AdminLayout({
         children,
