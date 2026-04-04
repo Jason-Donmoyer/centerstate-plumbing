a9b7ba7249bfc646a1b7a12e5c9f283d2aa7fd34
Jason Donmoyer <jasondonmoyer@Mac.lan>
2026-04-04 15:51:21 -0400

    Added 404-not-found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div className="flex flex-col min-h-screen items-center justify-center bg-brand-blue text-white">
+            <h1 className="text-2xl font-extrabold">404</h1>
+            <p className="font-semibold">Page Not Found</p>
+        </div>
+    )
+}
